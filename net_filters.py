d3a6c1242e005161c9c85c49caa586204b480307
Mclean Alexandre <alexandre.mclean@ubisoft.com>
2015-11-13 17:43:38 -0500

    Add exception handling for KeyError

--- net_filters.py
+++ net_filters.py
@@ -4,18 +4,24 @@ from netaddr import IPNetwork, IPAddress
 def find_ip(*a, **kw):
     facts = a[0]
     network = IPNetwork(a[1])
-    for addr_txt in facts['ansible_all_ipv4_addresses']:
-        if IPAddress(addr_txt) in network:
-            return addr_txt
+    try:
+        for addr_txt in facts['ansible_all_ipv4_addresses']:
+            if IPAddress(addr_txt) in network:
+                return addr_txt
+    except KeyError:
+        pass
     return None
 
 
 def find_ipnet(*a, **kw):
     facts = a[0]
     network = IPNetwork(a[1])
-    for addr_txt in facts['ansible_all_ipv4_addresses']:
-        if IPAddress(addr_txt) in network:
-            return addr_txt + "/" + str(network.prefixlen)
+    try:
+        for addr_txt in facts['ansible_all_ipv4_addresses']:
+            if IPAddress(addr_txt) in network:
+                return addr_txt + "/" + str(network.prefixlen)
+    except KeyError:
+        pass
     return None
 
 
@@ -23,10 +29,13 @@ def find_mac(*a, **kw):
     ip = find_ip(*a, **kw)
 
     facts = a[0]
-    for int_txt in facts['ansible_interfaces']:
-        key = 'ansible_' + int_txt.replace('-', '_')
-        if key in facts and 'ipv4' in facts[key] and facts[key]['ipv4']['address'] == ip:
-            return facts[key]['macaddress'].upper()
+    try:
+        for int_txt in facts['ansible_interfaces']:
+            key = 'ansible_' + int_txt.replace('-', '_')
+            if key in facts and 'ipv4' in facts[key] and facts[key]['ipv4']['address'] == ip:
+                return facts[key]['macaddress'].upper()
+    except KeyError:
+        pass
     return None
 
 
@@ -60,19 +69,23 @@ def find_netdev_with_bridge(facts, cidr, bridge, *args, **kwargs):
 def find_netdev(*a, **kw):
     facts = a[0]
     network = IPNetwork(a[1])
-    if ('br-ex' in facts['ansible_interfaces'] and
-        'ipv4' in facts['ansible_br_ex']):
+    
+    try:
+        if ('br-ex' in facts['ansible_interfaces'] and
+            'ipv4' in facts['ansible_br_ex']):
 
-        for netdev in facts['ansible_interfaces']:
-            if (netdev[:3] not in ['br-', 'ovs'] and
-                'ipv4' not in facts['ansible_' + netdev.replace('-', '_')]):
-                return netdev
-    else:
-        for netdev in facts['ansible_interfaces']:
-            devinfo = facts['ansible_' + netdev.replace('-', '_')]
-            if ('ipv4' in devinfo and
-                IPAddress(devinfo['ipv4']['address']) in network):
-                return netdev
+            for netdev in facts['ansible_interfaces']:
+                if (netdev[:3] not in ['br-', 'ovs'] and
+                    'ipv4' not in facts['ansible_' + netdev.replace('-', '_')]):
+                    return netdev
+        else:
+            for netdev in facts['ansible_interfaces']:
+                devinfo = facts['ansible_' + netdev.replace('-', '_')]
+                if ('ipv4' in devinfo and
+                    IPAddress(devinfo['ipv4']['address']) in network):
+                    return netdev
+    except KeyError:
+        pass
 
     return None
 
